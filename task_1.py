3a64297980b540d5094b0f94d369d214534ad1b5
Jackstorm9 <mohammedali091184@gmail.com>
2025-10-23 21:29:33 +0300

    Task 1 Done

---
+++ task_1.py
@@ -0,0 +1,9 @@
+# take input from the user three numbers and do multiplication and floor division and addition
+# use string concatenation (Meaning "Multiplication: 10, Division: 2, etc.")
+# searching up is allowed
+number1 = int(input("number1: "))
+number2 = int(input("number2: ")) 
+number3 = int(input("number3: "))
+print(f"multplication {number1 * number2}")
+print(f"addition {number2 + number3}")
+print(f"floor division {number2 // number2}")
